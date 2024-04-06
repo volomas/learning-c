cc9905b9ca86b103e418a28da4aa2ef80b5f9441
Volodymyr Masliy <thebestvova@gmail.com>
2024-04-06 21:37:24 +0200

    get

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -5,8 +5,8 @@
 typedef struct Node Node;
 
 struct Node {
-  //TODO generics? or how to support multiple types
-  // "void *"
+  // TODO generics? or how to support multiple types
+  //  "void *"
   int data;
   Node *next;
 };
@@ -17,37 +17,38 @@ struct LinkedList {
   Node *root;
 };
 
-LinkedList* create_linked_list() {
-  return (LinkedList*) malloc(sizeof(LinkedList));
+LinkedList *create_linked_list() {
+  return (LinkedList *)malloc(sizeof(LinkedList));
 }
 
 void append(LinkedList *list, int data) {
   if (NULL == list->root) {
-    list->root = (Node*)malloc(sizeof(Node));
+    list->root = (Node *)malloc(sizeof(Node));
     list->root->data = data;
   } else {
-    Node* last = list->root;
+    Node *last = list->root;
     while (NULL != last->next) {
       last = last->next;
     }
-    last->next = (Node*)malloc(sizeof(Node));
+    last->next = (Node *)malloc(sizeof(Node));
     last->next->data = data;
   }
   list->size++;
 }
 
 void prepend(LinkedList *list, int data) {
-  Node *newRoot = (Node*)malloc(sizeof(Node));
+  Node *newRoot = (Node *)malloc(sizeof(Node));
   newRoot->data = data;
   newRoot->next = list->root;
   list->root = newRoot;
   list->size++;
 }
 
-// TODO what if linked list is empty?
 int first(LinkedList *list) { 
-  return list->root->data; 
-}
+  if (list->root == NULL) {
+    return -1;
+  }
+  return list->root->data; }
 
 int last(LinkedList *list) {
   Node *last = list->root;
@@ -57,7 +58,19 @@ int last(LinkedList *list) {
   return last->data;
 }
 
-int get(LinkedList *list, int idx) { return 0; }
+int get(LinkedList *list, int idx) {
+  if (idx >= list->size) {
+    return -1;
+  }
+  int count = 0;
+
+  Node *last = list->root;
+  while (count != idx) {
+    last = last->next;
+    count++;
+  }
+  return last->data;
+}
 
 int size(LinkedList *list) { return list->size; }
 
@@ -70,6 +83,8 @@ int main(int argc, char *argv[]) {
   LinkedList *list = create_linked_list();
   printf("empty ll length: %d\n", size(list));
   assert(size(list) == 0);
+  assert(first(list) == -1);
+  assert(get(list, 0) == -1);
 
   append(list, 9);
   assert(size(list) == 1);
@@ -81,10 +96,11 @@ int main(int argc, char *argv[]) {
   assert(size(list) == 3);
   assert(last(list) == 3);
 
-
-  //TODO test prepend
   prepend(list, 1);
   assert(size(list) == 4);
   assert(first(list) == 1);
-  // assert(get(list, 1) == 9);
+  assert(get(list, 0) == 1);
+  assert(get(list, 1) == 9);
+  assert(get(list, 3) == last(list));
+  assert(get(list, 100) == -1);
 }
